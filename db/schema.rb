af1afc4272375c2db7fea49d1dc856941fe81a6d
Taylor Carr <Tate@Macintosh-3.local>
2008-11-12 21:07:49 -0500

    Fixed group update

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20081022234013) do
+ActiveRecord::Schema.define(:version => 20081113010936) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(:version => 20081022234013) do
     t.boolean  "active"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "leader_name"
+    t.string   "leader_email"
   end
 
 end
