7a810c7bfcce060dc902eca16da0195cd6f07b1e
Bradley J. Spaulding <brad.spaulding@gmail.com>
2008-12-16 14:17:08 -0500

    added nice handling of LdapError, when LDAP can't be found.

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -3,14 +3,19 @@ class LoginController < ApplicationController
   end
 
   def login
-    if !User.authenticate(params[:username], params[:password])
-      flash[:notice] = "Login Failed. Username/Password combination incorrect."
+    begin
+      if !User.authenticate(params[:username], params[:password])
+        flash[:notice] = "Login Failed. Username/Password combination incorrect."
+        redirect_to :action => 'index'
+      else
+        session[:username] = params[:username]
+        session[:password] = params[:password]
+        session[:last_activity_time] = Time.now.to_i
+        redirect_to :controller => 'groups', :action => 'index'
+      end
+    rescue Net::LDAP::LdapError
+      flash[:notice] = "Cannot connect to Directory Server. Please try again later."
       redirect_to :action => 'index'
-    else
-      session[:username] = params[:username]
-      session[:password] = params[:password]
-      session[:last_activity_time] = Time.now.to_i
-      redirect_to :controller => 'groups', :action => 'index'
     end
   end
 
